e7791a41fdc21b30b0065d164038a6a90d52b365
Lucas Carvalhães <lucascarber@gmail.com>
2020-08-28 08:21:54 -0300

    initial commit

---
+++ src/app/directives/for.directive.ts
@@ -0,0 +1,17 @@
+import { Directive, Input } from '@angular/core';
+
+@Directive({
+  selector: '[myFor]'
+})
+export class ForDirective implements OnInit {
+
+  @Input('myForEm') numbers: number[]
+
+  constructor() { 
+
+  }
+  
+  ngOnInit(): void {
+
+  }
+}
